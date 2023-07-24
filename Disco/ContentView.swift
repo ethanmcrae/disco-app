88805363d5aa80b69ce78d10087d0f0e4ce0bb26
Ethan McRae <ethanmcrae0@gmail.com>
2023-07-24 11:27:31 -0700

    Initial Commit

---
+++ Disco/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Disco
+//
+//  Created by Ethan McRae on 7/24/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Disco/DiscoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiscoApp.swift
+//  Disco
+//
+//  Created by Ethan McRae on 7/24/23.
+//
+
+import SwiftUI
+
+@main
+struct DiscoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
